3f5e55d385b33d658d5767c5eec443968ce37bab
nadavbm11 <nadavbm11@gmail.com>
2025-06-24 18:20:53 +0300

    add gifts classes

---
+++ src/IncreasingLife.cpp
@@ -0,0 +1,10 @@
+//-----include section-----
+#include "IncreasingLife.h"
+
+
+//-----functions section------
+//-----------------------------------------------------------------------------
+IncreasingLife::IncreasingLife(std::string name, const sf::Vector2f& position)
+    : ItemsToCollect(name, position)
+{
+}
---
+++ src/RemoveEnemy.cpp
@@ -0,0 +1,10 @@
+//-----include section-----
+#include "RemoveEnemy.h"
+
+
+//-----functions section------
+//-----------------------------------------------------------------------------
+RemoveEnemy::RemoveEnemy(std::string name, const sf::Vector2f& position)
+    : ItemsToCollect(name, position)
+{
+}
